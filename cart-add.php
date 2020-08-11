9ba3f165791ba6bbc448a4f7b45e59318d9d8ab7
AKdestroyer1 <53081999+AKdestroyer1@users.noreply.github.com>
2020-08-11 23:55:53 +0530

    Delete cart-add.php

--- cart-add.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'includes/common.php';
-$item_id= $_GET['id'];
-$user_id= $_SESSION['user_id']; 
-$insert_query = "INSERT INTO user_items(user_id, item_id, status) VALUES('$user_id', '$item_id', 'Added to cart')";
-$insert_result = mysqli_query($con, $insert_query) or die(mysqli_error($con));
-header('location: products.php');
-?>
-

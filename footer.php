596c824d19ac13f1534fe7f77a8f7fc207399865
AKdestroyer1 <53081999+AKdestroyer1@users.noreply.github.com>
2020-08-12 21:21:43 +0530

    Delete footer.php

--- footer.php
+++
@@ -1,5 +0,0 @@
-        <footer>
-            <div class="container">
-                <center>Copyright © Lifestyle Store. All Rights Reserved | Contact Us: +91 90000 00000</center>
-            </div>
-        </footer>
